da6b6a41f9c679b84e946ba11ea2e7d7e8352913
Esther Cui <ecui@uw.edu>
2023-03-11 14:55:33 -0800

    made all changes to app

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+import makeupfunctions
 
+from flask import Flask, render_template, request
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+
+# Create a view function for /
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Create a view function for /results
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    if request.method == 'POST':
+        brand = request.form['brand_name']
+        type = request.form['product_type']
+        price_range = request.form['price_range']
+        all_makeup = makeupfunctions.get_all_makeup()
+        products = makeupfunctions.combine_all_info(all_makeup, brand, type, price_range)
+        return render_template('results.html', brand=brand, type=type, products=products)
+    else:
+        return "Wrong HTTP method", 400
