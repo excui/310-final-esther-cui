860f345ce9bca810f80f9abc480ab7a6beff3831
Esther Cui <ecui@uw.edu>
2023-03-11 18:11:50 -0800

    small changes made to app

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ def index():
 @app.route('/results', methods=['GET', 'POST'])
 def results():
     if request.method == 'POST':
-        brand = request.form['brand_name']
+        brand_all_lower = request.form['brand_name']
+        brand = brand_all_lower.title()
         type = request.form['product_type']
         price_range = request.form['price_range']
         all_makeup = makeupfunctions.get_all_makeup()
